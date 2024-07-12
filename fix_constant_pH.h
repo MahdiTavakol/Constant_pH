01830f3232b5068801117b62629cd366b3ec40fa
MahdiTavakol <61837861+MahdiTavakol@users.noreply.github.com>
2024-07-12 16:42:14 +0100

    Update fix_constant_pH.h

--- fix_constant_pH.h
+++ fix_constant_pH.h
@@ -1,1 +1,56 @@
+/* -*- c++ -*- ----------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   https://www.lammps.org/, Sandia National Laboratories
+   LAMMPS development team: developers@lammps.org
 
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+/* ---v0.0.00----- */
+
+#ifdef FIX_CLASS
+// clang-format off
+FixStyle(constant_pH,FixConstantPH);
+// clang-format on
+#else
+
+#ifndef LMP_FIX_CONSTANTPH_H
+#define LMP_FIX_CONSTANTPH_H
+
+#include "fix.h"
+
+namespace LAMMPS_NS {
+
+	class FixConstantPH: public Fix {
+	public:
+		FixMetaAR(class LAMMPS*, int, char**);
+		~FixMetaAR() override;
+		int setmask() override;
+		void init() override;
+		void setup(int) override;
+		void pre_exchange(int) override;
+		double compute_scalar() override;
+		double compute_vector(int) override;
+		double memory_usage() override;
+		void init_list(int, class NeighList*) override;
+
+	private:
+		// Input variables for constant values
+    int igroupH, groupHbit;
+    
+
+
+
+
+
+
+	};
+
+}    // namespace LAMMPS_NS
+
+#endif
+#endif
