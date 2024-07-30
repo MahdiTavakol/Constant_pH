97f6cb0c8ec1e33a30feaedfecf6a417a17c9acb
MahdiTavakol <61837861+MahdiTavakol@users.noreply.github.com>
2024-07-30 16:25:10 +0100

    Update fix_constant_pH.cpp

--- fix_constant_pH.cpp
+++ fix_constant_pH.cpp
@@ -1,1 +1,49 @@
+// clang-format off
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   https://www.lammps.org/, Sandia National Laboratories
+   LAMMPS development team: developers@lammps.org
 
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#include "fix.h"
+
+#include "atom.h"
+#include "atom_masks.h"
+#include "error.h"
+#include "force.h"
+#include "group.h"
+#include "memory.h"
+
+#include <cstring>
+
+using namespace LAMMPS_NS;
+using namespace FixConst;
+
+/* ---------------------------------------------------------------------- */
+
+FixConstantPH::FixConstantPH(LAMMPS *lmp, int narg, char **arg):
+  Fix(lmp, narg, arg)
+{
+  if (narg < ??) utils::mising_cmd_args(FLERR,"fix constant pH", error);
+  nevery = utils::inumeric(FLERR,arg[3],false,lmp);
+  if (nevery < 0) error->all(FLERR,"Ilegal fix constant pH every value {}", nevery);
+
+  int iarg = 4;
+  while (iarg < narg) {
+    
+  }
+}
+
+/* ---------------------------------------------------------------------- */
+
+FixConstantPH::init()
+{
+  
+}
