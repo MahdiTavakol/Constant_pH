10b7654a2e38372c3687cd23210a1b26fb1d4676
MahdiTavakol <61837861+MahdiTavakol@users.noreply.github.com>
2024-07-30 19:30:17 +0100

    Update fix_constant_pH.cpp

--- fix_constant_pH.cpp
+++ fix_constant_pH.cpp
@@ -43,7 +43,47 @@ FixConstantPH::FixConstantPH(LAMMPS *lmp, int narg, char **arg):
 
 /* ---------------------------------------------------------------------- */
 
-FixConstantPH::init()
+void FixConstantPH::init()
 {
   
+}
+
+/* ---------------------------------------------------------------------- */
+
+void FixConstantPH::integrate_lambda()
+{
+   double f_lambda = -(HB-HA + df_lambda*R*T*ln(10)*(pK-pH) + dU);
+   double a_lambda = f_lambda / m_lambda;
+   double t_lambda = nevery*update->dt;
+   double H_lambda = (1-lambda)*HA + lambda*HB + f_lambda*R*T*ln(10)*(pK-pH) + U + (m_lambda/2.0)*(v_lambda**2);
+   lambda = (1.0/2.0)*(a_lambda)*(t_lambda)**2 + v_lambda*t_lambda + lambda;
+   v_lambda = a_lambda * t_lambda + v_lambda;
+}
+/* ---------------------------------------------------------------------- */
+
+void FixConstantPH::calculate_df(const double lambda)
+{
+   f_lambda = 1.0/(1+exp(-50*(x-0.5));
+   df_lambda = 50*exp(-50*(x-0.5))/(f_lambda*f_lambda);
+}
+
+/* ----------------------------------------------------------------------- */
+
+void FixConstantPH::calculate_dU(const double lambda)
+{
+   double U1, U2, U3, U4, U5;
+   double dU1, dU2, dU3, dU4, dU5;
+   U1 = -k*exp(-(lambda-1-b)*(lambda-1-b)/(2*a*a));
+   U2 = -k*exp(-(lambda+b)*(lambda+b)/(2*a*a));
+   U3 = d*exp(-(lambda-0.5)*(lambda-0.5)/(2*s*s));
+   U4 = 0.5*w*(1-erff(r*(lambda+m));
+   U5 = 0.5*w*(1+erff(r*(lambda-1-m));
+   dU1 = -((lambda-1-b)/(2*a*a))*U1;
+   dU2 = -((lambda+b)/(2*a*a))*U2;
+   dU3 = -((lambda-0.5)/(s*s))*U3;
+   dU4 = -0.5*w*r*2*exp(-r*r*(lambda+0.5)*(lambda+0.5))/sqrt(pi);
+   dU5 = 0.5*w*r*2*exp(-r*r*(lambda-1-m)*(lambda-1-m))/sqrt(pi);
+
+    U =  U1 +  U2 +  U3 +  U4 +  U5;
+   dU = dU1 + dU2 + dU3 + dU4 + dU5;
 }
