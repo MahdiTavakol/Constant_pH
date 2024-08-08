2c9eb20de683e4344d74e310ebde120805ba0f08
MahdiTavakol <61837861+MahdiTavakol@users.noreply.github.com>
2024-08-08 14:40:21 +0100

    Update fix_constant_pH.cpp

--- fix_constant_pH.cpp
+++ fix_constant_pH.cpp
@@ -38,17 +38,15 @@ FixConstantPH::FixConstantPH(LAMMPS *lmp, int narg, char **arg):
 {
   if (narg < 9) utils::mising_cmd_args(FLERR,"fix constant_pH", error);
   nevery = utils::inumeric(FLERR,arg[3],false,lmp);
-  if (nevery < 0) error->all(FLERR,"Ilegal fix constant pH every value {}", nevery);
-  igroupH = group->find(arg[4]);
-  if (igroupH == -1) error->all(FLERR,"Cannot find the hydrogens group for fix constant_pH);
-  groupHbit = group->bitmask(igroupH);
+  if (nevery < 0) error->all(FLERR,"Illegal fix constant_pH every value {}", nevery);
+  typeH = utils::inumeric(FLERR,arg[4],flase,lmp);
+  if (typeH > atom->ntypes) error->all(FLERR,"Illegal fix constant_pH atom type {}",typeH); 
   igroupW = group->find(arg[5]);
   if (igroupW == -1) error->all(FLERR,"Cannot find the hydronium ions hydrogen groups for fix constant_pH");
   // For hydronium the initial charges are qO=-0.834, qH1=0.611, qH2=0.612, qH3=0.611 (based on TIP3P water model)
   // if m is the number of igroupW atoms, m-1 atoms in the igroupW should be given a charge of lambda/3.0 
   // and then the last atom must be given the charge of m*lambda/3 - (m-1)*lambda/3. This is done to count for rounding errors
-  if (group->count(igroupW) != 3 * group->count(igroupH)) 
-     error->all(FLERR, "Number of hydronium hydrogen atoms must be three times the number of protonable hydrogens");
+  warning->all(FLERR, "Number of hydronium hydrogen atoms must be three times the number of protonable hydrogens");
   groupWbit = group->bitmask(igroupW);
   pK = utils::numeric(FLERR, arg[6], false, lmp);
   pH = utils::numeric(FLERR, arg[7], false, lmp);
@@ -65,6 +63,11 @@ FixConstantPH::FixConstantPH(LAMMPS *lmp, int narg, char **arg):
          error->one(FLERR, "Cannot find fix constant_pH the GFF correction file {}",arg[iarg+1]);
        iarg = iarg + 2;
     }
+    if ((strcmp(arg[iarg],"qs") == 0)
+    {
+       qHW = utils::numeric(FLERR, arg[iarg+1]);
+       iarg = iarg + 1;
+    }
     else
        error->all(FLERR, "Unknown fix constant_pH keyword: {}", arg[iarg]);
   }
@@ -77,7 +80,6 @@ FixConstantPH::~FixConstantPH()
 {
    memory->destroy(epsilon_init); 
    memory->destroy(GFF);
-   delete [] q_init;
 
    if (fp && (comm->me == 0)) fclose(fp);
 }
@@ -125,10 +127,10 @@ void FixConstantPH::init()
     void *ptr1 = pair1->extract(pparam1,pdim1);
     if (ptr1 == nullptr)
         error->all(FLERR,"Fix constant_pH pair style param not supported");
-    if (pdim != 2)
+    if (pdim1 != 2)
          error->all(FLERR,"Pair style parameter {} is not compatible with fix constant_pH", pparam1);
    
-    if (pdim == 2) double ** epsilon = (double **) ptr;
+    if (pdim1 == 2) double ** epsilon = (double **) ptr1;
 
     int ntypes = atom->ntypes;
     memory->create(epsilon_init,ntypes+1,ntypes+1,"constant_pH:epsilon_init");
@@ -138,12 +140,10 @@ void FixConstantPH::init()
          for (int j = i; j <= ntypes+1; j++)
              epsilon_init[i][j] = epsilon[i][j];
 
-    int nmax = atom->nmax;
-    q_init = new double[nmax];
-    for (int i = 0; i < nlocal; i++)
-	q_init[i] = q[i];
 
-    if (GFF_flag == true)
+
+    GFF_lambda = 0.0;
+    if (GFF_flag)
 	init_GFF();
 }
 
@@ -151,19 +151,21 @@ void FixConstantPH::init()
 
 void FixConstantPH::integrate_lambda()
 {
-   double f_lambda = -(HB-HA + df*R*T*ln(10)*(pK-pH) + dU);
-   double a_lambda = f_lambda / m_lambda;
-   double t_lambda = nevery*update->dt;
-   double H_lambda = (1-lambda)*HA + lambda*HB + f*R*T*ln(10)*(pK-pH) + U + (m_lambda/2.0)*(v_lambda**2);
-   lambda = (1.0/2.0)*(a_lambda)*(t_lambda)**2 + v_lambda*t_lambda + lambda;
-   v_lambda = a_lambda * t_lambda + v_lambda;
+   if (GFF_flag) calculate_GFF();
+   double  f_lambda = -(HB-HA + df*R*T*ln(10)*(pK-pH) + dU - GFF_lambda);
+   double  a_lambda = f_lambda / m_lambda;
+   double dt_lambda = update->dt;
+   double  H_lambda = (1-lambda)*HA + lambda*HB + f*R*T*ln(10)*(pK-pH) + U + (m_lambda/2.0)*(v_lambda**2); // This might not be needed. May be I need to tally this into energies.
+   // I might need to use the leap-frog integrator and so this function might need to be in other functions than postforce()
+   lambda += (1.0/2.0)*(a_lambda)*(dt_lambda)*(dt_lambda) + v_lambda*dt_lambda;
+   v_lambda += a_lambda * dt_lambda;
 }
 /* ---------------------------------------------------------------------- */
 
 void FixConstantPH::calculate_df()
 {
    f = 1.0/(1+exp(-50*(lambda-0.5));
-   df = 50*exp(-50*(lambda-0.5))/(f**2);
+   df = 50*exp(-50*(lambda-0.5))/(f*f);
 }
 
 /* ----------------------------------------------------------------------- */
@@ -192,14 +194,6 @@ void FixConstantPH::calculate_dU()
 template <int stage>
 void FixConstantPH::compute_Hs()
 {
-   double **f = atom->f;
-   int *mask = atom->mask;
-   int nlocal = atom->nlocal;
-
-   invoked_vector = update->ntimestep;
-
-
-
    if (stage == 0)
    {
       if (atom->nmax > nmax) {    // reallocate working arrays if necessary
@@ -232,7 +226,7 @@ void FixConstantPH::compute_Hs()
 
 void FixConstantPH::allocate_storage()
 {
-  nmax = atom->nmax;
+  int nmax = atom->nmax;
   memory->create(f_orig, nmax, 3, "constant_pH:f_orig");
   memory->create(peatom_orig, nmax, "constant_pH:peatom_orig");
   memory->create(pvatom_orig, nmax, 6, "constant_pH:pvatom_orig");
@@ -337,20 +331,22 @@ void FixConstantPH::modify_params(const double& scale)
 {
     int nlocal = atom->nlocal;
     int * mask = atom->mask;
+    int * type = atom->type;
     int ntypes = atom->ntypes;
 
     // not sure about the range of these two loops
     for (int i = 0; i < ntypes + 1; i++)
 	for (int j = i; j < ntypes + 1; j++)
-	    epsilon[i][j] = epsilon_init[i][j] * scale;
+	    if (type[i] == typeH || type[j] == typeH)
+	    	epsilon[i][j] = epsilon_init[i][j] * scale;
 
     int numWH = group->count(igroupW);
     for (int i = 0; i < nlocal; i++)
     {
-        if (mask[i] & groupHbit)
-	    q[i] = q_init[i] * scale;
+        if (type[i] == typeH)
+	    q[i] = scale;
 	if (mask[i] & groupWbit)
-	    q[i] = q_init[i] * (1-scale) * (float) numWH /3.0;	
+	    q[i] = qHW + (-scale) * 1.0 /3.0;	
      }
 	    
 	    
@@ -363,8 +359,8 @@ void FixConstantPH::modify_params(const double& scale)
    ---------------------------------------------------------------------- */
 
 void FixConstantPH::update_lmp() {
-   eflag = 1;
-   vflag = 0;
+   int eflag = 1;
+   int vflag = 0;
    timer->stamp();
    if (force->pair && force->pair->compute_flag) {
      force->pair->compute(eflag, vflag);
@@ -389,8 +385,8 @@ void FixConstantPH::init_GFF()
    char line[100];
    fgets(line,sizeof(line),fp);
    line[strcspn(line,"\n")] = 0;
-   GFF_array_size = atoi(line);
-   memory->create(GFF,GFF_array_size,2,"constant_pH:GFF");
+   GFF_size = atoi(line);
+   memory->create(GFF,GFF_size,2,"constant_pH:GFF");
    int i = 0;
    while(fgets(line,sizeof(line), fp) != NULL && i < GFF_array_size)
       {
@@ -420,19 +416,19 @@ void FixConstantPH::init_GFF()
 void FixConstantPH::calculate_GFF()
 {
    int i = 0;
-   while (i < GFF_array_size && GFF_array[i][0] < lambda) i++;
+   while (i < GFF_size && GFF[i][0] < lambda) i++;
 
    if (i == 0)
    {
       error->warning(FLERR,"Warning lambda of {} in Fix constant_pH out of the range, it usually should not happen",lambda);
-      GFF = GFF_array[0][1] + ((GFF_array[1][1]-GFF_array[0][1])/(GFF_array[1][0]-GFF_array[0][0]))*(lambda - GFF_array[0][0]);
+      GFF_lambda = GFF[0][1] + ((GFF[1][1]-GFF[0][1])/(GFF[1][0]-GFF[0][0]))*(lambda - GFF[0][0]);
    }
-   if (i > 0 && i < GFF_array_size - 1)
-      GFF = GFF_array[i-1] + ((FF_array[i][1]-GFF_array[i-1][1])/(FF_array[i][0]-GFF_array[i-1][0]))*(lambda - GFF_array[i-1][0]);
-   if (i == GFF_array_size - 1)
+   if (i > 0 && i < GFF_size - 1)
+      GFF_lambda = GFF[i-1] + ((GFF[i][1]-GFF[i-1][1])/(GFF[i][0]-GFF[i-1][0]))*(lambda - GFF[i-1][0]);
+   if (i == GFF_size - 1)
    {
       error->warning(FLERR,"Warning lambda of {} in Fix constant_pH out of the range, it usually should not happen",lambda);
-      GFF = GFF_array[i] + ((FF_array[i][1]-GFF_array[i-1][1])/(FF_array[i][0]-GFF_array[i-1][0]))*(lambda - GFF_array[i][0]);
+      GFF_lambda = GFF[i] + ((GFF[i][1]-GFF[i-1][1])/(GFF[i][0]-GFF[i-1][0]))*(lambda - GFF[i][0]);
    }
 }
    
@@ -441,8 +437,24 @@ void FixConstantPH::calculate_GFF()
    memory usage of local atom-based array --> Needs to be updated at the end
    ---------------------------------------------------------------------- */
 
-double ComputePEAtom::memory_usage()
+double FixConstantPH::memory_usage()
 {
+
+		double *peatom_orig, **pvatom_orig;
+ 		double energy_orig;
+ 		double kvirial_orig[6];
+		double *keatom_orig, **kvatom_orig;
+
+	
+  int nmax = atom->nmax;
+  int nlocal = atom->nlocal;
+  int ntypes = atom->ntypes;
+  double pair_bytes = sizeof(Pair);
+  double GFF_bytes = 2.0 * GFF_size * sizeof(double);
+  double epsilon_init = (double) (ntypes + 1) * (double) (ntypes + 1) * sizeof(double);
+  double q_orig_bytes = (double) nmax * sizeof(double);
+  double f_orig_bytes = (double) nmax * 3.0 * sizeof(double);
+  double 
   double bytes = (double) nmax * sizeof(double);
   return bytes;
 }
