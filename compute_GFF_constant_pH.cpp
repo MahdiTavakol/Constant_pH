7aa203af1ec38d110798c4714a5efb507dce60de
Mahdi Tavakol <61837861+MahdiTavakol@users.noreply.github.com>
2024-11-22 10:03:10 +0000

    Update compute_GFF_constant_pH.cpp

--- compute_GFF_constant_pH.cpp
+++ compute_GFF_constant_pH.cpp
@@ -1,1 +1,31 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   https://www.lammps.org/, Sandia National Laboratories
+   LAMMPS development team: developers@lammps.org
 
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+
+#include "compute_GFF_constant_pH"
+
+#include "atom.h"
+#include "comm.h"
+#include "domain.h"
+#include "error.h"
+#include "fix.h"
+#include "force.h"
+#include "input.h"
+#include "kspace.h"
+#include "memory.h"
+#include "modify.h"
+#include "pair.h"
+#include "pair_hybrid.h"
+#include "timer.h"
+#include "update.h"
+#include "variable.h"
