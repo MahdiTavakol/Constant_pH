b3e1effcdccd4a9ab14e022db952d2a4d4ab0de0
Mahdi Tavakol <61837861+MahdiTavakol@users.noreply.github.com>
2024-11-26 17:29:19 +0000

    Update fix_nh_constant_pH.h

--- fix_nh_constant_pH.h
+++ fix_nh_constant_pH.h
@@ -15,6 +15,28 @@
 #define LMP_FIX_NH_CONSTANT_PH_H
 
 #include "fix.h"    // IWYU pragma: export
+#include "fix_constant_pH.h"
+#include "fix_nh.h"
+
+namespace LAMMPS_NS {
+
+class FixNHConstantPH : public FixNH {/* -*- c++ -*- ----------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   https://www.lammps.org/, Sandia National Laboratories
+   LAMMPS development team: developers@lammps.org
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifndef LMP_FIX_NH_CONSTANT_PH_H
+#define LMP_FIX_NH_CONSTANT_PH_H
+
+#include "fix.h"    // IWYU pragma: export
 #include "fix_nh.h"
 
 namespace LAMMPS_NS {
@@ -33,6 +55,30 @@ class FixNHConstantPH : public FixNH {
 
   // lambda variables from the fix constant pH  
   Fix *fix_constant_pH;
+  char *fix_constant_pH_id;
+  double* x_lambdas, *v_lambdas, *a_lambdas, *m_lambdas;
+  int n_lambdas;
+
+};
+
+}    // namespace LAMMPS_NS
+
+#endif
+
+ public:
+  FixNHConstantPH(class LAMMPS *, int, char **);
+  ~FixNHConstantPH() override;
+  void init() override;
+  double memory_usage() override;
+
+ protected:
+
+  void nve_x() override;
+  void nve_v() override;
+  void nh_v_temp() override;
+
+  // lambda variables from the fix constant pH  
+  FixConstantPH *fix_constant_pH;
   char *fix_constant_pH_id;
   double* x_lambdas, *v_lambdas, *a_lambdas, *m_lambdas;
   int n_lambdas;
