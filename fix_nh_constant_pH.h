a5632f5ce3a2ca7196843143454e4a8ddefce878
Mahdi Tavakol <61837861+MahdiTavakol@users.noreply.github.com>
2024-11-26 11:28:58 +0000

    Update fix_nh_constant_pH.h

--- fix_nh_constant_pH.h
+++ fix_nh_constant_pH.h
@@ -10,3 +10,32 @@
 
    See the README file in the top-level LAMMPS directory.
 ------------------------------------------------------------------------- */
+
+#ifndef LMP_FIX_NH_CONSTANT_PH_H
+#define LMP_FIX_NH_CONSTANT_PH_H
+
+#include "fix.h"    // IWYU pragma: export
+
+namespace LAMMPS_NS {
+
+class FixNHConstantPH : public FixNH {
+ public:
+  FixNHConstantPH(class LAMMPS *, int, char **);
+  ~FixNHConstantPH() override;
+  void init() override;
+  void setup(int) override;
+  double memory_usage() override;
+
+ protected:
+  double lambda, v_lambda, t_lambda;
+
+  void nve_x() override;
+  void nve_v() override;
+  void nh_v_temp() override;
+  virtual int size_restart_global();
+
+};
+
+}    // namespace LAMMPS_NS
+
+#endif
