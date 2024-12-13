efe68391eb61cb6e434bc70e05ae40e094bb5251
Mahdi Tavakol <61837861+MahdiTavakol@users.noreply.github.com>
2024-12-13 15:42:16 +0000

    Update fix_adaptive_protonation.h

--- fix_adaptive_protonation.h
+++ fix_adaptive_protonation.h
@@ -25,51 +25,60 @@ FixStyle(AdaptiveProtonation,FixAdaptiveProtonation);
 
 namespace LAMMPS_NS {
 
-	class FixAdaptiveProtonation : public Fix {
-	public:
-		FixAdaptiveProtonation(class LAMMPS*, int, char**);
-		~FixAdaptiveProtonation() override;
-		int setmask() override;
-		void init() override;
-		void setup(int) override;
-		void pre_exchange(int) override;
-		double compute_scalar() override;
-		double compute_vector(int) override;
-		double memory_usage() override;
-		void init_list(int, class NeighList*) override;
+   class FixAdaptiveProtonation : public Fix {
+   public:
+      FixAdaptiveProtonation(class LAMMPS*, int, char**);
+      ~FixAdaptiveProtonation() override;
+      int setmask() override;
+      void init() override;
+      void setup(int) override;
+      void pre_exchange(int) override;
+      double compute_scalar() override;
+      double compute_vector(int) override;
+      double memory_usage() override;
+      void init_list(int, class NeighList*) override;
 
-	private:
-		// Input variables for constant values
-		int typeP, typeO, typeH, typeOw, typeHw;
-    		double threshold;
+   private:
+      // Input variables for constant values
+      int typeP, typeO, typeH, typeOH;
+      double threshold;
 
-		// pKa and pH values
-		int pKa, pH;
+      // pKa and pH values
+      int pKa, pH;
     
 
-		// Input variables for variable values
-		char* typePstr, *typeOstr, *typeSstr;
+      // Input variables for variable values
+      char* typePstr, *typeOstr, *typeSstr, *typeOHstr, *typePOHstr;
 
-		//
-		int typePvar, typeOvar, typeHvar;
-    		int typePstyle, typeOstyle, typeSstyle;
-
-
-		// Neighborlist is required for accessing neighbors
-		class NeighList* list;
+      //
+      int typePvar, typeOvar, typeHvar, typeOHvar, typePOHvar;
+      int typePstyle, typeOstyle, typeSstyle, typeOHstyle, typePOHstyle;
 
 
-		// mark 0  -> not type P
-		// mark 1  -> in water or on surface (protonable)
-		// mark -1 -> in solid
-		int * mark;
+      // Neighborlist is required for accessing neighbors
+      class NeighList* list;
 
 
-		void mark_protonation_deprotonation();
+      // mark 0  -> not type P
+      // mark 1  -> in water or on surface (protonable)
+      // mark -1 -> in solid
+      int * mark;
 
-    		int nmax;
+      int nmax;
 
-	};
+
+      // Setting the same molecule id for atoms connected to each other
+      void set_molecule_id();
+      // Mark phosphate atoms for protonation/deprotonation
+      void mark_protonation_deprotonation();
+      // Add/Remove hydrogens for all the phosphates in the system based on the output of the mark_protonation_deprotonation() function
+      void modify_protonable_hydrogens();
+      // Add hydrogen atoms to the phosphate with a P atom having an index of i
+      void add_hydrogens(const int& i, const int& req_numHs, const int oAtoms[3], const int hAtoms[3]);
+      // Remove hydrogen atoms from the phosphate with a P atom having an index of i
+      void remove_hydrogens(const int& i, int& numHs_2_del, const int oAtoms[3], const int hAtoms[3]);
+
+   };
 
 }    // namespace LAMMPS_NS
 
