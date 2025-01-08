182b70b2a8a8f70c4c3b682e999dd818623daa9d
Mahdi Tavakol <61837861+MahdiTavakol@users.noreply.github.com>
2025-01-08 10:43:32 +0000

    Update fix_constant_pH.h

--- fix_constant_pH.h
+++ fix_constant_pH.h
@@ -29,6 +29,7 @@ namespace LAMMPS_NS {
 
   class FixConstantPH: public Fix {
      friend class FixNHConstantPH;
+     friend class ComputeGFF;
      public:
 	FixConstantPH(class LAMMPS*, int, char**);
 	~FixConstantPH() override;
