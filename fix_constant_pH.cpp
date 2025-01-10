2859aa10c4a29e027cb1340babda3924b5141cd6
Mahdi Tavakol <61837861+MahdiTavakol@users.noreply.github.com>
2025-01-10 12:13:23 +0000

    Update fix_constant_pH.cpp

--- fix_constant_pH.cpp
+++ fix_constant_pH.cpp
@@ -235,12 +235,12 @@ void FixConstantPH::setup(int /*vflag*/)
     w = 200;
     s = 0.3;
     h = 7.0;
-    k = 2*4.417; //2.553;
+    k = 4.417; //2.553;
     a = 0.04208; //0.03401;
     b = 0.002957; //0.005238;
     r = 16.458; 
     m = 0.1507;
-    d = 30.0; //2.0; //The height of the barrier is 2*d
+    d = 35.0; //2.0; //The height of the barrier is 2*d
 
     // default values for the buffer potential with h = 0 from Donnin J Chem Theory Comput 2016 - Table S2
     w_buff = 200;
