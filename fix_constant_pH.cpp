ccde8ffc779ce2f7e3a5d60e6fa2f25217989389
Mahdi Tavakol <61837861+MahdiTavakol@users.noreply.github.com>
2025-01-17 17:08:56 +0000

    Update fix_constant_pH.cpp

--- fix_constant_pH.cpp
+++ fix_constant_pH.cpp
@@ -241,7 +241,7 @@ void FixConstantPH::setup(int /*vflag*/)
     b = 0.002957; //0.005238;
     r = 16.458; 
     m = 0.1507;
-    d = 6.00; //3.5
+    d = 7.00; //3.5
 
     // default values for the buffer potential with h = 0 from Donnin J Chem Theory Comput 2016 - Table S2
     w_buff = 200;
