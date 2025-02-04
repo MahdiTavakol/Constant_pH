69cd4671a4491c3c55c5d9fa55d570a036e30277
Mahdi Tavakol <61837861+MahdiTavakol@users.noreply.github.com>
2025-02-04 16:49:37 +0000

    Update fix_adaptive_protonation.cpp

--- fix_adaptive_protonation.cpp
+++ fix_adaptive_protonation.cpp
@@ -10,7 +10,7 @@
 
    See the README file in the top-level LAMMPS directory.
 ------------------------------------------------------------------------- */
-/* ---------- v0.08.56----------------- */
+/* ---------- v0.10.00----------------- */
 // Please remove unnecessary includes 
 #include "fix_adaptive_protonation.h"
 
