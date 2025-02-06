2b7f5d17633dc264a793e56b29aaba0592cdb04b
Mahdi Tavakol <61837861+MahdiTavakol@users.noreply.github.com>
2025-02-06 14:18:51 +0000

    Update compute_solute_coordination.cpp
    Added a build_one() function to the compute_solute_coordination.cpp

--- compute_solute_coordination.cpp
+++ compute_solute_coordination.cpp
@@ -83,11 +83,76 @@ ComputeSoluteCoordination::~ComputeSoluteCoordination()
 void ComputeSoluteCoordination::init()
 {
    // Request a fulintl neighbor list
-   int list_flags = NeighConst::REQ_OCCASIONAL | NeighConst::REQ_FULL;
+   int list_flags = NeighConst::REQ_OCCASIONAL; // | NeighConst::REQ_FULL;
 
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   https://www.lammps.org/, Sandia National Laboratories
+   LAMMPS development team: developers@lammps.org
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+/* ---------- v0.10.02----------------- */
+// Please remove unnecessary includes 
+#include "compute_solute_coordination.h"
+
+#include "atom.h"
+#include "atom_masks.h"
+#include "domain.h"
+#include "error.h"
+#include "input.h"
+#include "math_const.h"
+#include "memory.h"
+#include "comm.h"
+#include "modify.h"
+#include "region.h"
+#include "neigh_list.h"
+#include "neighbor.h"
+#include "output.h"
+#include "respa.h"
+#include "update.h"
+#include "variable.h"
+
+
+#include "force.h"
+#include "pair.h"
+#include "improper.h"
+#include "dihedral.h"
+#include "kspace.h"
+#include "angle.h"
+#include "bond.h"
+#include "atom.h"
+#include "group.h"
+
+#include "thermo.h"
+#include <cmath>
+#include <cstring>
+#include <stdio.h>
+
+using namespace LAMMPS_NS;
+using namespace MathConst;
+
+
+/* --------------------------------------------------------------------------------------- */
+
+ComputeSoluteCoordination::ComputeSoluteCoordination(LAMMPS* lmp, int narg, char** arg) : Compute(lmp, narg, arg) 
+{
+   if (narg < 3) utils::missing_cmd_args(FLERR, "fix adaptive_protonation", error);
+
+   peratom_flag = 1;
+   scalar_flag = 0;
+   extscalar = 0;
+   size_peratom_cols = 0;
 
    // request for a neighbor list
    neighbor->add_request(this, list_flags);
+
+
 }
 
 /* ---------------------------------------------------------------------------------------
@@ -103,6 +168,9 @@ void ComputeSoluteCoordination::init_list(int /*id*/, NeighList* ptr)
 
 void ComputeSoluteCoordination::compute_peratom()
 {
+   // Building the neighbor
+  neighbor->build_one(list);
+ 
   int* type = atom->type;
   invoked_peratom = update->ntimestep;
 
