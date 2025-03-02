6c8c99550afd42d7cad0ccf68f9d16d135a60054
Mahdi Tavakol <61837861+MahdiTavakol@users.noreply.github.com>
2025-03-02 11:36:45 +0000

    Update fix_adaptive_protonation.h

--- fix_adaptive_protonation.h
+++ fix_adaptive_protonation.h
@@ -27,7 +27,6 @@ FixStyle(adaptive_protonation,FixAdaptiveProtonation);
 namespace LAMMPS_NS {
 
    class FixAdaptiveProtonation : public Fix {
-   friend FixConstantPH;
    public:
       FixAdaptiveProtonation(class LAMMPS*, int, char**);
       ~FixAdaptiveProtonation() override;
