621068fe3cc8a8d2d0526ec2a35e88f08d7fd020
Mahdi Tavakol <61837861+MahdiTavakol@users.noreply.github.com>
2025-03-12 15:22:02 +0000

    Update fix_constant_pH.cpp

--- fix_constant_pH.cpp
+++ fix_constant_pH.cpp
@@ -11,7 +11,7 @@
 
    See the README file in the top-level LAMMPS directory.
 ------------------------------------------------------------------------- */
-/* ---v0.10.27----- */
+/* ---v0.10.28----- */
 
 #define DEBUG
 #ifdef DEBUG
